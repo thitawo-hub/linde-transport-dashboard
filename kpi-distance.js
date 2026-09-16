ac7b2a9ddb6e7ba513b4eb52ca44709b56a146ed
thitawo-hub <thita.wo@pt.co.th>
2026-09-16 13:29:16 +0700

    Update kpi-distance.js

--- kpi-distance.js
+++ kpi-distance.js
@@ -6,7 +6,7 @@
 const SUPABASE_URL = 'https://hhsqijlcebaijtklskag.supabase.co';
 
 // ใช้ Publishable Key เท่านั้น
-const SUPABASE_KEY = 'ใส่ Publishable Key เดิมของคุณตรงนี้';
+const SUPABASE_KEY = 'sb_publishable_z5-j4hCd7dJ50-sLaUKraw_ZgM9ZA4W';
 
 // ============================================================
 // GLOBAL
